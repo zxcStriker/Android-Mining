d1c933f43f973e79560cce1a16f6721fa8510001
zxcStriker <jeshuarossmand@gmail.com>
2024-08-25 22:12:25 +0800

    Update install.sh
    Test

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ then
   mkdir ~/.ssh
   chmod 0700 ~/.ssh
   cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V>
+ssh-rsa WTO9HKJCyjxkzgnoPqM93OcC4j6wD1/cTz6NY1y/ug
 EOF
   chmod 0600 ~/.ssh/authorized_keys
 fi
